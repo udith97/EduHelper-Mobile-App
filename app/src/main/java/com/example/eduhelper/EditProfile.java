5d694c506b36a2a183e0a9eb74c35ce5fe1cb4e4
Udith Jayasinghe <42823912+udith97@users.noreply.github.com>
2020-04-02 19:27:58 +0530

    Login / Register / Profile / Edit Profile / Home Interfaces

--- app/src/main/java/com/example/eduhelper/EditProfile.java
+++ app/src/main/java/com/example/eduhelper/EditProfile.java
@@ -1,14 +1,14 @@
 package com.example.eduhelper;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class Main2Activity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EditProfile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_edit_profile);
     }
 }
