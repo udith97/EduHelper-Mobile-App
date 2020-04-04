43b2207a3cc3cc98bc3d846296e08766dec0d5b3
KAvinda Geeth <kavindageeth00@gmail.com>
2020-04-05 00:28:48 +0530

    edit announcement layout

---
+++ app/src/main/java/com/example/eduhelper/Edit_announcement.java
@@ -0,0 +1,14 @@
+package com.example.eduhelper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Edit_announcement extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_announcement);
+    }
+}
