a3b41b03ac756304652cb94b9ec6b0f59e5e61ab
Ravindu Jayakody <48220639+ravinduJ@users.noreply.github.com>
2020-04-09 23:05:10 +0530

    User Interfaces Updated Past papers.

---
+++ app/src/main/java/com/example/eduhelper/Edit_Past_past_paper.java
@@ -0,0 +1,14 @@
+package com.example.eduhelper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Edit_Past_past_paper extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit__past_past_paper);
+    }
+}
---
+++ app/src/main/java/com/example/eduhelper/Insert_P_Paper.java
@@ -0,0 +1,14 @@
+package com.example.eduhelper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Insert_P_Paper extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert__p__paper);
+    }
+}
