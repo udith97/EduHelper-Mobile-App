1f09b23786b34d23d7fcda5d648453fbf36cfd10
Udith Jayasinghe <42823912+udith97@users.noreply.github.com>
2020-04-13 20:02:07 +0530

    Registration Backend final

--- app/src/main/java/com/example/eduhelper/Register.java
+++ app/src/main/java/com/example/eduhelper/Register.java
@@ -1,12 +1,20 @@
 package com.example.eduhelper;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Register extends AppCompatActivity {
@@ -31,6 +39,71 @@ public class Register extends AppCompatActivity {
         rGoToLogin = findViewById(R.id.goToLogin);
 
         fAth = FirebaseAuth.getInstance();
+
+        if (fAth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
+
+        rRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String idNo = rIdNo.getText().toString().trim();
+                String name = rName.getText().toString().trim();
+                String email = rEmail.getText().toString().trim();
+                String faculty = rFaculty.getText().toString().trim();
+                String year = rYear.getText().toString().trim();
+                String password = rPassword.getText().toString().trim();
+                String conPassword = rConPassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(idNo)){
+                    rIdNo.setError("ID Number is required.");
+                    return;
+                }
+                if (TextUtils.isEmpty(name)){
+                    rName.setError("Name is required.");
+                    return;
+                }
+                if (TextUtils.isEmpty(email)){
+                    rEmail.setError("Email is required.");
+                    return;
+                }
+                if (TextUtils.isEmpty(faculty)){
+                    rFaculty.setError("Faculty is required.");
+                    return;
+                }
+                if (TextUtils.isEmpty(year)){
+                    rYear.setError("Year is required.");
+                    return;
+                }
+                if (TextUtils.isEmpty(password)){
+                    rPassword.setError("Password is required.");
+                    return;
+                }if (password.length() < 6){
+                    rPassword.setError("Password mus be greater than six characters.");
+                    return;
+                }
+                if (password != conPassword){
+                    rConPassword.setError("Password not matched.");
+                    return;
+                }
+
+                //register the user in firebase
+
+                fAth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(Register.this,"User Created.", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        }else{
+                            Toast.makeText(Register.this, "Error !" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+            }
+        });
 
     }
 }
