936fef9b87606f5e5688ba16959a0d42e1b2d61d
Udith Jayasinghe <42823912+udith97@users.noreply.github.com>
2020-04-16 16:44:14 +0530

    Changing Layerouts to leaniyer and login backend

--- app/src/main/java/com/example/eduhelper/MainActivity.java
+++ app/src/main/java/com/example/eduhelper/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.eduhelper;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void logout (View view){
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getApplicationContext(),Login.class));
+        finish();
     }
 }
