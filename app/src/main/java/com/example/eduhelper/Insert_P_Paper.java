c4eaabf5be48502a4c2e321b35412e6cc968a386
Ravindu Jayakody <48220639+ravinduJ@users.noreply.github.com>
2020-04-19 04:20:46 +0530

    backend Updated for Choose file button.

--- app/src/main/java/com/example/eduhelper/Insert_P_Paper.java
+++ app/src/main/java/com/example/eduhelper/Insert_P_Paper.java
@@ -1,7 +1,11 @@
 package com.example.eduhelper;
 
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,8 +18,11 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class Insert_P_Paper extends AppCompatActivity {
 
+    private static final int PICK_IMG_REQUEST = 1;
     private EditText year,faculty, moduleCode, exam, semester;
-    private Button submit;
+    private Button submit, choosefile;
+
+    private Uri fileuri;
 
     private FirebaseDatabase rootNode;
     private DatabaseReference mDatabase;
@@ -31,7 +38,11 @@ public class Insert_P_Paper extends AppCompatActivity {
         moduleCode = findViewById(R.id.ip_modulecode);
         exam = findViewById(R.id.ip_exam);
         faculty = findViewById(R.id.ip_faculty);
+
+
         submit = findViewById(R.id.ipSubmit);
+        choosefile = findViewById(R.id.ip_choosefile_btn);
+
 
 
         submit.setOnClickListener(new View.OnClickListener() {
@@ -39,16 +50,6 @@ public class Insert_P_Paper extends AppCompatActivity {
             public void onClick(View v) {
                 rootNode = FirebaseDatabase.getInstance();
                 mDatabase = rootNode.getReference("pastPapers");
-
-//                int iyear = Integer.parseInt(year.getEditableText().toString());
-//                int isem = Integer.parseInt(semester.getEditableText().toString());
-//                String mcode= moduleCode.getEditableText().toString();
-//                String ifac= faculty.getEditableText().toString();
-//                String iexam = exam.getEditableText().toString();
-//
-//                pastpaperHelper  pastpaperHelper = new pastpaperHelper(iyear, isem, mcode, ifac, iexam);
-//
-//                mDatabase.child(mcode).setValue(pastpaperHelper);
 
                 int iyear = Integer.parseInt(year.getText().toString());
                 int sem = Integer.parseInt(semester.getText().toString());
@@ -58,21 +59,38 @@ public class Insert_P_Paper extends AppCompatActivity {
 
                 pastpaperHelper paperHelper = new pastpaperHelper(iyear, sem, mcode, ifac, iexam);
 
-
                 mDatabase.child(mcode).setValue(paperHelper);
-
-
 
             }
         });
 
 
+        choosefile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenFileChooser();
+            }
+        });
+    }
 
 
+    private void OpenFileChooser(){
+        Intent intent = new Intent();
+
+        intent.setType("file/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent,PICK_IMG_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == PICK_IMG_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null){
+
+            fileuri = data.getData();
 
 
-
-
-
+        }
     }
 }
