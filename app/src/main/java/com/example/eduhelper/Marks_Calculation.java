904fe4407611e43e055540d1a267b03f27416610
Randeepa Bandara <42823841+Randeepa97@users.noreply.github.com>
2020-04-18 20:06:13 +0530

    fix dropdown and calculation

--- app/src/main/java/com/example/eduhelper/Marks_Calculation.java
+++ app/src/main/java/com/example/eduhelper/Marks_Calculation.java
@@ -4,13 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class Marks_Calculation extends AppCompatActivity {
+public class Marks_Calculation extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-    private EditText num1;
+     private EditText num1;
      private EditText num2;
      private Button cal;
      private TextView result;
@@ -19,6 +23,14 @@ public class Marks_Calculation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_marks__calculation);
+
+      Spinner spinner = findViewById(R.id.spinner);
+      ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.modules,android.R.layout.simple_spinner_item);
+      adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+      spinner.setAdapter(adapter);
+      spinner.setOnItemSelectedListener(this);
+
+
 
       num1 = (EditText) findViewById(R.id.ent1);
         num2 = (EditText) findViewById(R.id.ent2);
@@ -29,9 +41,10 @@ public class Marks_Calculation extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
-                int number1 = Integer.parseInt(num1.getText().toString());
-                int number2 = Integer.parseInt(num2.getText().toString());
-                int sum = number1+number2;
+
+                double number1 = Double.parseDouble(num1.getText().toString());
+                double number2 = Double.parseDouble(num2.getText().toString());
+                double sum = 45 - (number1+number2);
                 result.setText("Answer :" + String.valueOf(sum));
 
 
@@ -39,6 +52,19 @@ public class Marks_Calculation extends AppCompatActivity {
             }
         });
 
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 }
