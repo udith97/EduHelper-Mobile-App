880fcfbdb4936a4cb2b3ea5550dacc261440fe46
Ravindu Jayakody <48220639+ravinduJ@users.noreply.github.com>
2020-04-19 03:22:32 +0530

    connected to firebase realtime database and inserted data into database.

--- app/src/main/java/com/example/eduhelper/Insert_P_Paper.java
+++ app/src/main/java/com/example/eduhelper/Insert_P_Paper.java
@@ -3,12 +3,76 @@ package com.example.eduhelper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 public class Insert_P_Paper extends AppCompatActivity {
+
+    private EditText year,faculty, moduleCode, exam, semester;
+    private Button submit;
+
+    private FirebaseDatabase rootNode;
+    private DatabaseReference mDatabase;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert__p__paper);
+
+        year = findViewById(R.id.ip_year);
+        semester = findViewById(R.id.ip_semester);
+        moduleCode = findViewById(R.id.ip_modulecode);
+        exam = findViewById(R.id.ip_exam);
+        faculty = findViewById(R.id.ip_faculty);
+        submit = findViewById(R.id.ipSubmit);
+
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rootNode = FirebaseDatabase.getInstance();
+                mDatabase = rootNode.getReference("pastPapers");
+
+//                int iyear = Integer.parseInt(year.getEditableText().toString());
+//                int isem = Integer.parseInt(semester.getEditableText().toString());
+//                String mcode= moduleCode.getEditableText().toString();
+//                String ifac= faculty.getEditableText().toString();
+//                String iexam = exam.getEditableText().toString();
+//
+//                pastpaperHelper  pastpaperHelper = new pastpaperHelper(iyear, isem, mcode, ifac, iexam);
+//
+//                mDatabase.child(mcode).setValue(pastpaperHelper);
+
+                int iyear = Integer.parseInt(year.getText().toString());
+                int sem = Integer.parseInt(semester.getText().toString());
+                String mcode = moduleCode.getText().toString();
+                String ifac = faculty.getText().toString();
+                String iexam = exam.getText().toString();
+
+                pastpaperHelper paperHelper = new pastpaperHelper(iyear, sem, mcode, ifac, iexam);
+
+
+                mDatabase.child(mcode).setValue(paperHelper);
+
+
+
+            }
+        });
+
+
+
+
+
+
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/eduhelper/pastpaperHelper.java
@@ -0,0 +1,63 @@
+package com.example.eduhelper;
+
+public class pastpaperHelper {
+
+    private int year;
+    private int semester;
+    private String ModuleCode;
+    private String faculty;
+    private String exam;
+
+    public pastpaperHelper() {
+    }
+
+    public pastpaperHelper(int year, int semester, String moduleCode, String faculty, String exam) {
+        this.year = year;
+        this.semester = semester;
+        ModuleCode = moduleCode;
+        this.faculty = faculty;
+        this.exam = exam;
+    }
+
+
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getSemester() {
+        return semester;
+    }
+
+    public void setSemester(int semester) {
+        this.semester = semester;
+    }
+
+    public String getModuleCode() {
+        return ModuleCode;
+    }
+
+    public void setModuleCode(String moduleCode) {
+        ModuleCode = moduleCode;
+    }
+
+    public String getFaculty() {
+        return faculty;
+    }
+
+    public void setFaculty(String faculty) {
+        this.faculty = faculty;
+    }
+
+    public String getExam() {
+        return exam;
+    }
+
+    public void setExam(String exam) {
+        this.exam = exam;
+    }
+}
