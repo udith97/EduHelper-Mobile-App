d360817ee471597990e26e090153b2d6a0b3562b
Udith Jayasinghe <42823912+udith97@users.noreply.github.com>
2020-04-19 16:39:54 +0530

    Lecturer Function Interfaces

---
+++ app/src/main/java/com/example/eduhelper/lecturerHelper.java
@@ -0,0 +1,61 @@
+package com.example.eduhelper;
+
+public class lecturerHelper {
+
+    private String name;
+    private String moduleCode;
+    private String location;
+    private String  email;
+    private String contact;
+
+    public lecturerHelper() {
+    }
+
+    public lecturerHelper(String name, String moduleCode, String location, String email, String contact) {
+        this.name = name;
+        this.moduleCode = moduleCode;
+        this.location = location;
+        this.email = email;
+        this.contact = contact;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getModuleCode() {
+        return moduleCode;
+    }
+
+    public void setModuleCode(String moduleCode) {
+        this.moduleCode = moduleCode;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+}
