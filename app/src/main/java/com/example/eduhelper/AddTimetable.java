b69afab2e6d02efd10558ff4cd268d8be3d0167b
Randeepa Bandara <42823841+Randeepa97@users.noreply.github.com>
2020-04-19 19:39:42 +0530

    Backend updated timetable

--- app/src/main/java/com/example/eduhelper/AddTimetable.java
+++ app/src/main/java/com/example/eduhelper/AddTimetable.java
@@ -1,10 +1,31 @@
 package com.example.eduhelper;
 
+
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class AddTimetable extends AppCompatActivity {
+
+
+    private static final int PICK_IMG_REQUEST = 1;
+
+    private EditText faculty , year, semester ,group;
+    private Button submit, choosefile;
+
+    private Uri fileuri;
+
+    private FirebaseDatabase rootNode;
+    private DatabaseReference refdb;
+
 
 
 
@@ -13,7 +34,96 @@ public class AddTimetable extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_timetable);
 
+        faculty = (EditText) findViewById(R.id.in1);
+        year = findViewById(R.id.in2);
+        semester = findViewById(R.id.in3);
+        group = findViewById(R.id.in4);
 
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                rootNode = FirebaseDatabase.getInstance();
+                refdb = rootNode.getReference("timetables");
+
+
+                refdb.setValue("alalf");
+
+
+
+
+            }
+        });
+
+
+
+
+
+//        faculty = (EditText) findViewById(R.id.in1);
+//        year = findViewById(R.id.in2);
+//        semester = findViewById(R.id.in3);
+//        group = findViewById(R.id.in4);
+//
+//        submit = findViewById(R.id.btn_submit);
+//        choosefile = findViewById(R.id.btn_browse);
+
+//        submit.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                rootNode = FirebaseDatabase.getInstance();
+//                mDatabase = rootNode.getReference("timetables");
+////
+////                String tfaculty = faculty.getText().toString();
+////                String tyear = year.getText().toString();
+////                String tsemester = semester.getText().toString();
+////                String tgroup = group.getText().toString();
+////
+////                timetablehelper Timetablehelper = new timetablehelper(tfaculty, tyear, tsemester, tgroup);
+//
+////                mDatabase.child(tyear).setValue(Timetablehelper);
+////                mDatabase.setValue("time table");
+//
+//
+//            }
+//        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        final EditText Input = findViewById(R.id.in1);
+        final Button Clear = findViewById(R.id.clr_btn);
+
+
+
+        Clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               String Text = Input.getText().toString();
+               if (Text.isEmpty()) {
+                   Toast.makeText(getApplicationContext(), "Already Empty", Toast.LENGTH_SHORT).show();
+               }else{
+                   Input.setText("");
+
+
+               }
+            }
+        });
 
 
     }
---
+++ app/src/main/java/com/example/eduhelper/timetablehelper.java
@@ -0,0 +1,51 @@
+package com.example.eduhelper;
+
+public class timetablehelper {
+
+    private String tfaculty;
+    private String tyear;
+    private String tsemester;
+    private String tgroup;
+
+    public timetablehelper() {
+    }
+
+    public timetablehelper(String tfaculty, String tyear, String tsemester, String tgroup) {
+        this.tfaculty = tfaculty;
+        this.tyear = tyear;
+        this.tsemester = tsemester;
+        this.tgroup = tgroup;
+    }
+
+    public String getTfaculty() {
+        return tfaculty;
+    }
+
+    public void setTfaculty(String tfaculty) {
+        this.tfaculty = tfaculty;
+    }
+
+    public String getTyear() {
+        return tyear;
+    }
+
+    public void setTyear(String tyear) {
+        this.tyear = tyear;
+    }
+
+    public String getTsemester() {
+        return tsemester;
+    }
+
+    public void setTsemester(String tsemester) {
+        this.tsemester = tsemester;
+    }
+
+    public String getTgroup() {
+        return tgroup;
+    }
+
+    public void setTgroup(String tgroup) {
+        this.tgroup = tgroup;
+    }
+}
