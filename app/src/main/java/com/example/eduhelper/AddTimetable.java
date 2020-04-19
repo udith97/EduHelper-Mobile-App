d9af176cd2784f38d9cacf8608a60619d269a1ad
Randeepa Bandara <42823841+Randeepa97@users.noreply.github.com>
2020-04-19 21:23:06 +0530

    Backend addtimetable

--- app/src/main/java/com/example/eduhelper/AddTimetable.java
+++ app/src/main/java/com/example/eduhelper/AddTimetable.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -50,27 +51,40 @@ public class AddTimetable extends AppCompatActivity {
                String tgroup = group.getText().toString();
                timetablehelper Timetablehelper = new timetablehelper(tfaculty, tyear, tsemester, tgroup);
 
-               mDatabase.child(tyear).setValue(Timetablehelper);
+               if(mDatabase != null) {
+                   mDatabase.child(tyear).setValue(Timetablehelper);
+                   Toast.makeText(AddTimetable.this,"Insert Successful", Toast.LENGTH_LONG).show();
+//                   startActivity(new Intent(getApplicationContext(), ));
+               }else{
+                   Toast.makeText(AddTimetable.this,"Input Failed", Toast.LENGTH_LONG).show();
+
+               }
 
             }
         });
 
 //        final EditText Input = findViewById(R.id.in1);
-//        final Button Clear = findViewById(R.id.clr_btn);
-//
-//        Clear.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//               String Text = Input.getText().toString();
-//               if (Text.isEmpty()) {
-//                   Toast.makeText(getApplicationContext(), "Already Empty", Toast.LENGTH_SHORT).show();
-//               }else{
-//                   Input.setText("");
-//
-//
-//               }
-//            }
-//        });
+        final Button Clear = findViewById(R.id.clr_btn);
+
+        Clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               String Text = faculty.getText().toString();
+               String Text2 = year.getText().toString();
+                String Text3 = semester.getText().toString();
+                String Text4 = group.getText().toString();
+               if (Text.isEmpty() && Text2.isEmpty() && Text3.isEmpty() && Text4.isEmpty()) {
+                   Toast.makeText(getApplicationContext(), "Already Empty", Toast.LENGTH_SHORT).show();
+               }else{
+                   faculty.setText("");
+                   semester.setText("");
+                   year.setText("");
+                   group.setText("");
+
+
+               }
+            }
+        });
 
 
     }
