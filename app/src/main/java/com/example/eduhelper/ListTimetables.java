a52cee35d536ad72a9af0d3a49fb6fa287e671f0
Randeepa Bandara <42823841+Randeepa97@users.noreply.github.com>
2020-04-22 19:48:22 +0530

    back end final final

--- app/src/main/java/com/example/eduhelper/ListTimetables.java
+++ app/src/main/java/com/example/eduhelper/ListTimetables.java
@@ -1,6 +1,9 @@
 package com.example.eduhelper;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.SearchView;
 
 import androidx.annotation.NonNull;
@@ -21,12 +24,15 @@ public class ListTimetables extends AppCompatActivity {
     private ArrayList<timetablehelper> thelper;
     private  RecyclerView recyclerView;
     private TimetableAdapterClass timetableAdapterClass;
+    private Button goInsert;
     SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_timetables);
+
+        goInsert = findViewById(R.id.btn_ins);
 
 
         recyclerView = findViewById(R.id.rv1);
@@ -37,6 +43,14 @@ public class ListTimetables extends AppCompatActivity {
         ref.addListenerForSingleValueEvent(valueEventListener);
 
         searchView = findViewById(R.id.searchView);
+
+        goInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),AddTimetable.class));
+            }
+        });
+
     }
 
     ValueEventListener valueEventListener = new ValueEventListener() {
