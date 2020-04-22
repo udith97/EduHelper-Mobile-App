147ac14998d7996cb53fc3ec0d596a199cfab543
Ravindu Jayakody <48220639+ravinduJ@users.noreply.github.com>
2020-04-22 19:32:57 +0530

    Update, delete, create and delete page linked with input page

--- app/src/main/java/com/example/eduhelper/PaperAdapter.java
+++ app/src/main/java/com/example/eduhelper/PaperAdapter.java
@@ -1,13 +1,20 @@
 package com.example.eduhelper;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
@@ -32,12 +39,37 @@ public class PaperAdapter extends RecyclerView.Adapter<PaperAdapter.PaperHolder>
 
     @Override
     public void onBindViewHolder(@NonNull PaperHolder holder, int position) {
-        PastpaperHelper pastpaperHelper = this.userData.get(position);
+        final PastpaperHelper pastpaperHelper = this.userData.get(position);
         holder.pyear.setText(pastpaperHelper.getYear());
         holder.pMcode.setText(pastpaperHelper.getModuleCode());
         holder.psem.setText(pastpaperHelper.getSemester());
         holder.pfac.setText(pastpaperHelper.getFaculty());
         holder.pexam.setText(pastpaperHelper.getExam());
+
+        holder.deletebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("pastPapers").child(pastpaperHelper.getYear());
+                databaseReference.removeValue();
+                Toast.makeText(context,"Record deleted", Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+        holder.uploadbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent x = new Intent(v.getContext(), PaperUpdate.class);
+                x.putExtra("year", pastpaperHelper.getYear() );
+                x.putExtra("moduleCode", pastpaperHelper.getModuleCode());
+                x.putExtra("semester", pastpaperHelper.getSemester());
+                x.putExtra("faculty", pastpaperHelper.getFaculty());
+                x.putExtra("exam", pastpaperHelper.getExam());
+
+                v.getContext().startActivity(x);
+            }
+        });
+
     }
 
     @Override
@@ -48,6 +80,8 @@ public class PaperAdapter extends RecyclerView.Adapter<PaperAdapter.PaperHolder>
     public class PaperHolder extends  RecyclerView.ViewHolder{
 
         TextView pyear, pMcode,psem, pfac, pexam;
+        Button deletebtn, uploadbtn;
+        CardView cardView;
 
         public PaperHolder(@NonNull View itemView) {
             super(itemView);
@@ -57,6 +91,9 @@ public class PaperAdapter extends RecyclerView.Adapter<PaperAdapter.PaperHolder>
             psem = itemView.findViewById(R.id.vsem);
             pfac = itemView.findViewById(R.id.vfac);
             pexam = itemView.findViewById(R.id.vexam);
+            deletebtn = itemView.findViewById(R.id.vdeletebtn);
+            cardView = itemView.findViewById(R.id.holderview);
+            uploadbtn = itemView.findViewById(R.id.vupdate);
 
         }
     }
