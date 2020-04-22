5b8a10e3df62b33fdbfc22ef84f99aee46667d06
Udith Jayasinghe <42823912+udith97@users.noreply.github.com>
2020-04-22 13:50:15 +0530

    Retrive Data Backend error fix Update Data Backend Implementation Delete Data Backend Implementation

--- app/src/main/java/com/example/eduhelper/List_Lecturers.java
+++ app/src/main/java/com/example/eduhelper/List_Lecturers.java
@@ -2,10 +2,10 @@ package com.example.eduhelper;
 
 import android.os.Bundle;
 import android.widget.SearchView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -18,8 +18,9 @@ import java.util.ArrayList;
 
 public class List_Lecturers extends AppCompatActivity {
     DatabaseReference ref;
-    ArrayList<lecturerHelper> list;
-    RecyclerView recyclerView;
+    private ArrayList<lecturerHelper> lecHelper;
+    private  RecyclerView recyclerView;
+    private LecturerAdapterClass lecturerAdapterClass;
     SearchView searchView;
 
     @Override
@@ -27,68 +28,32 @@ public class List_Lecturers extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list__lecturers);
 
-       ref = FirebaseDatabase.getInstance().getReference().child("lecturer");
+
        recyclerView = findViewById(R.id.rv);
+       recyclerView.setLayoutManager(new LinearLayoutManager(this));
+       lecHelper = new ArrayList<lecturerHelper>();
+
+        ref = FirebaseDatabase.getInstance().getReference().child("lecturer");
+        ref.addListenerForSingleValueEvent(valueEventListener);
+
        searchView = findViewById(R.id.searchView);
-
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        if (ref != null){
-            ref.addValueEventListener(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                    if(dataSnapshot.exists())
-                    {
-                        list = new ArrayList<>();
-                        for(DataSnapshot ds : dataSnapshot.getChildren())
-                        {
-                              list.add(ds.getValue(lecturerHelper.class));
-                        }
-                         LecturerAdapterClass lecturerAdapterClass = new LecturerAdapterClass(list);
-                         recyclerView.setAdapter(lecturerAdapterClass);
-                    }
-                }
-
-                @Override
-                public void onCancelled(@NonNull DatabaseError databaseError) {
-                    Toast.makeText(List_Lecturers.this, databaseError.getMessage(), Toast.LENGTH_SHORT).show();
-
-                }
-            });
-        }
-        if (searchView != null)
-        {
-            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-                @Override
-                public boolean onQueryTextSubmit(String query) {
-                    return false;
-                }
-
-                @Override
-                public boolean onQueryTextChange(String s) {
-                    search(s);
-                    return true;
-                }
-            });
-        }
-
-    }
-
-    private void search(String str)
-    {
-        ArrayList<lecturerHelper> myList = new ArrayList<>();
-        for (lecturerHelper object : list)
-        {
-            if (object.getName().toLowerCase().contains(str.toLowerCase()))
-            {
-                myList.add(object);
+    ValueEventListener valueEventListener = new ValueEventListener() {
+        @Override
+        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            for (DataSnapshot dataSnapshot1: dataSnapshot.getChildren()){
+                lecturerHelper helper = dataSnapshot1.getValue(lecturerHelper.class);
+                lecHelper.add(helper);
             }
+            lecturerAdapterClass = new LecturerAdapterClass(List_Lecturers.this,lecHelper);
+            recyclerView.setAdapter(lecturerAdapterClass);
         }
-        LecturerAdapterClass lecturerAdapterClass = new LecturerAdapterClass(myList);
-        recyclerView.setAdapter(lecturerAdapterClass);
 
-    }
+        @Override
+        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+        }
+    };
+
 }
