c3205230270375586483c2b13fb312c8d840b799
Randeepa Bandara <42823841+Randeepa97@users.noreply.github.com>
2020-04-22 15:38:25 +0530

    backend final

--- app/src/main/java/com/example/eduhelper/ListTimetables.java
+++ app/src/main/java/com/example/eduhelper/ListTimetables.java
@@ -2,10 +2,10 @@ package com.example.eduhelper;
 
 import android.os.Bundle;
 import android.widget.SearchView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -16,10 +16,11 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
-class ListTimetables extends AppCompatActivity {
+public class ListTimetables extends AppCompatActivity {
     DatabaseReference ref;
-    ArrayList<timetablehelper> list1;
-    RecyclerView recyclerView;
+    private ArrayList<timetablehelper> thelper;
+    private  RecyclerView recyclerView;
+    private TimetableAdapterClass timetableAdapterClass;
     SearchView searchView;
 
     @Override
@@ -27,68 +28,32 @@ class ListTimetables extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_timetables);
 
-        ref = FirebaseDatabase.getInstance().getReference().child("timetables");
+
         recyclerView = findViewById(R.id.rv1);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        thelper = new ArrayList<timetablehelper>();
+
+        ref = FirebaseDatabase.getInstance().getReference().child("timetables");
+        ref.addListenerForSingleValueEvent(valueEventListener);
+
         searchView = findViewById(R.id.searchView);
-
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        if (ref != null){
-            ref.addValueEventListener(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                    if(dataSnapshot.exists())
-                    {
-                        list1 = new ArrayList<>();
-                        for(DataSnapshot ds : dataSnapshot.getChildren())
-                        {
-                            list1.add(ds.getValue(timetablehelper.class));
-                        }
-                        TimetableAdapterClass timetableAdapterClass = new TimetableAdapterClass(list1);
-                        recyclerView.setAdapter(timetableAdapterClass);
-                    }
-                }
-
-                @Override
-                public void onCancelled(@NonNull DatabaseError databaseError) {
-                    Toast.makeText(ListTimetables.this, databaseError.getMessage(), Toast.LENGTH_SHORT).show();
-
-                }
-            });
-        }
-        if (searchView != null)
-        {
-            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-                @Override
-                public boolean onQueryTextSubmit(String query) {
-                    return false;
-                }
-
-                @Override
-                public boolean onQueryTextChange(String s) {
-                    search(s);
-                    return true;
-                }
-            });
-        }
-
-    }
-
-    private void search(String str)
-    {
-        ArrayList<timetablehelper> myList1 = new ArrayList<>();
-        for (timetablehelper object : list1)
-        {
-            if (object.getTyear().toLowerCase().contains(str.toLowerCase()))
-            {
-                myList1.add(object);
+    ValueEventListener valueEventListener = new ValueEventListener() {
+        @Override
+        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            for (DataSnapshot dataSnapshot1: dataSnapshot.getChildren()){
+                timetablehelper helper = dataSnapshot1.getValue(timetablehelper.class);
+                thelper.add(helper);
             }
+            timetableAdapterClass = new TimetableAdapterClass(ListTimetables.this,thelper);
+            recyclerView.setAdapter(timetableAdapterClass);
         }
-        TimetableAdapterClass timetableAdapterClass = new TimetableAdapterClass(myList1);
-        recyclerView.setAdapter(timetableAdapterClass);
 
-    }
+        @Override
+        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+        }
+    };
+
 }
